25b186d74d3d5b9bbb020a91089e3f3f72e5cbfe
pawelch96 <pawelchmiel.20.03@gmail.com>
2019-10-23 23:47:19 +0200

    shop page with collection preview and collection item components

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from "react";
 
-import './collection-preview.styles.scss';
+import CollectionItem from "../collection-item/collection-item.component.jsx";
 
-const CollectionPreview = ({title, items}) => (
-    <div className='collection-preview'>
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <div className='preview'>
-            {
-                items.map((item =>
-                    <div key={item.id}>{item.name}</div>
-                ))
-            }
-        </div>
+import "./collection-preview.styles.scss";
+
+const CollectionPreview = ({ title, items }) => (
+  <div className="collection-preview">
+    <h1 className="title">{title.toUpperCase()}</h1>
+    <div className="preview">
+      {items
+        .filter((item, index) => index < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
+  </div>
 );
 
 export default CollectionPreview;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -10,19 +10,19 @@ class ShopPage extends React.Component {
 
         this.state = {
             collections: SHOP_DATA
-        }
-    };
+        };
+    }
 
     render() {
         const {collections} = this.state;
         return (<div className='shop-page'>
             {
-                collections.map((id, ...otherCollectionProps) =>(
+                collections.map(({id, ...otherCollectionProps}) =>(
                     <CollectionPreview key={id} {...otherCollectionProps} />
                 ))
             }
         </div>);
-    };
+    }
 }
 
 export default ShopPage;
