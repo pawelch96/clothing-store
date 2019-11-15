075a870ffb983f34582b1823bdf520c3bec0f5fb
Pawel Chmiel <p.chmiel@x-coding.pl>
2019-11-15 15:35:42 +0100

    collections data moved to firebase database, fetching data implemented

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,15 +1,15 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 
 const config = {
-  apiKey: "AIzaSyBbqo2-5JNEuje5FpgELY_Qe_gEnTw0fOM",
-  authDomain: "clthng-store.firebaseapp.com",
-  databaseURL: "https://clthng-store.firebaseio.com",
-  projectId: "clthng-store",
-  storageBucket: "clthng-store.appspot.com",
-  messagingSenderId: "588984577852",
-  appId: "1:588984577852:web:c56e2b1bffeeac9d9c3839"
+  apiKey: 'AIzaSyBbqo2-5JNEuje5FpgELY_Qe_gEnTw0fOM',
+  authDomain: 'clthng-store.firebaseapp.com',
+  databaseURL: 'https://clthng-store.firebaseio.com',
+  projectId: 'clthng-store',
+  storageBucket: 'clthng-store.appspot.com',
+  messagingSenderId: '588984577852',
+  appId: '1:588984577852:web:c56e2b1bffeeac9d9c3839'
 };
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
@@ -30,11 +30,44 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log("error creating user", error.message);
+      console.log('error creating user', error.message);
     }
   }
 
   return userReference;
+};
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(element => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, element);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = collections => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 firebase.initializeApp(config);
@@ -43,7 +76,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
